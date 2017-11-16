ffcf481688b6ee3d32621f014f9254ac61941223
cehel <celine.heldner@zuehlke.com>
2017-11-16 16:46:34 +0100

    add RXJava for room

--- app/src/main/java/ch/zuehlke/sbb/reddit/data/source/local/RedditPostsDataDao.kt
+++ app/src/main/java/ch/zuehlke/sbb/reddit/data/source/local/RedditPostsDataDao.kt
@@ -5,6 +5,7 @@ import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
 import ch.zuehlke.sbb.reddit.models.RedditPostsData
+import io.reactivex.Flowable
 
 /**
  * Created by celineheldner on 16.11.17.
@@ -14,6 +15,9 @@ interface RedditPostsDataDao{
 
     @Query("select * from redditPosts where parentPermaLink = :arg0 order by ordering")
     fun getPosts(permalink: String?): List<RedditPostsData>
+
+    @Query("select * from redditPosts where parentPermaLink = :arg0 order by ordering")
+    fun getPostsFlowable(permalink: String?): Flowable<List<RedditPostsData>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun addPostItems(item: List<RedditPostsData>)
