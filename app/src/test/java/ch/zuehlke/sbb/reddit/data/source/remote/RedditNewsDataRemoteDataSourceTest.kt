780b756c8731c1824562565d0053169f83b7b16c
cehel <celine.heldner@zuehlke.com>
2017-12-11 17:18:59 +0100

    test RXJAVA2 Testing

--- app/src/test/java/ch/zuehlke/sbb/reddit/data/source/remote/RedditNewsDataRemoteDataSourceTest.kt
+++ app/src/test/java/ch/zuehlke/sbb/reddit/data/source/remote/RedditNewsDataRemoteDataSourceTest.kt
@@ -2,14 +2,17 @@ package ch.zuehlke.sbb.reddit.data.source.remote
 
 import ch.zuehlke.sbb.reddit.createBaseModule
 import ch.zuehlke.sbb.reddit.data.source.RedditDataSource
+import ch.zuehlke.sbb.reddit.models.RedditNewsData
 import ch.zuehlke.sbb.reddit.models.RedditPostsData
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.instance
 import io.reactivex.observers.TestObserver
+import io.reactivex.subscribers.TestSubscriber
 import org.junit.Assert
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by celineheldner on 11.12.17.
@@ -30,11 +33,19 @@ class RedditNewsDataRemoteDataSourceTest {
 
     @Test
     fun getNews() {
+        //given
+        val subscriber = TestSubscriber<List<RedditNewsData>>()
+
+        //when
+        redditRemoteData.news.subscribe(subscriber)
+        subscriber.awaitDone(3,TimeUnit.SECONDS) //Wartet Terminal Event und bricht die Sequenz ab nach dem Timeout
+
+        //then
+        subscriber.assertNever(ArrayList<RedditNewsData>())
+        Assert.assertTrue(subscriber.values().size>0)
+        Assert.assertTrue(subscriber.values().get(0).size>0)
     }
 
-    @Test
-    fun refreshNews() {
-    }
 
     @Test
     fun posts() {
@@ -48,8 +59,7 @@ class RedditNewsDataRemoteDataSourceTest {
         observer.assertComplete()
         observer.assertNoErrors()
         Assert.assertTrue(observer.values().size>0)
-
-
+        Assert.assertTrue(observer.values().get(0).size>0)
     }
 
 }
